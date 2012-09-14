1c39656c7865324669baf28e583c5159de4cbf87
Juan Carlos Picado <juan@encuestame.org>
2012-09-14 23:56:17 +0200

    fist module

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,6 +1,9 @@
-require(["jquery", "jquery.alpha", "jquery.beta"], function($) {
+require(["jquery", "jquery.alpha", "jquery.beta"], function($, b, c) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
+    	console.log("jquery", $);
+    	console.log("jquery1", b);
+    	console.log("jquery2", c);
         $('body').alpha().beta();
     });
 });
